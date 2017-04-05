f0662c63ba6e498b796ea35650c4d46d27865d38
shermin <wing561@gmail.com>
2017-04-05 14:41:15 -0400

    updated README to include background.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# seq-sim
+#Seq-sim
+
+##Introduction
+The aim of Seq-sim is to help further our understanding of the
+selective pressure to maintain a protein coding sequences. The central
+dogma of biology is that DNA -> RNA -> Protein. Therefore, given a
+starting protein coding sequence and a mutation rate, seq-sim
+simulates mutations accumulating over many generations. If at any
+point, the protein coding portion is disrupted, the sequence is
+considered "dead" or non-functional. If the given sequence is known to
+be functional, the ratio of functional to non-functional sequences
+measures the selective pressure on the sequence. A ratio > 1 indicates
+low selective pressure (genetic drift); a ratio < 1 indicates pressure
+to maintain the coding sequence.
+
+Seq-sim simulates sequence evolution using a Markov process. The user
+needs to specify the mutation rate and the number of generation to
+simulate. The Markov process is At each generation, we use a Poisson
+random number to determine how many mutations to apply. For each
+mutation, we assume that the base can mutate to any other base with
+equal probability. The output of the program is the number of
+funtional and non-functional sequences.
+
+##How to run the program
+Coming soon...
+
--- seq_object.py
+++ seq_object.py
@@ -3,18 +3,27 @@ import seq_markov as mc
 from collections import Counter
 
 
-uniform_nt_probs = [0.25, 0.25, 0.25, 0.25]
-int_to_nt = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
-nt_to_int = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+UNIFORM_NT_PROBS = [0.25, 0.25, 0.25, 0.25]
+INT_TO_NT = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
+NT_TO_INT = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
 
-jc = {"A": {"C": 1.0/3, "G": 1.0/3, "T": 1.0/3},
+JC = {"A": {"C": 1.0/3, "G": 1.0/3, "T": 1.0/3},
       "C": {"A": 1.0/3, "G": 1.0/3, "T": 1.0/3},
       "G": {"A": 1.0/3, "C": 1.0/3, "T": 1.0/3},
       "T": {"A": 1.0/3, "C": 1.0/3, "G": 1.0/3}}
 
 
-class sequence():
-    def __init__(self, inseq, mutation_rate=10**-9, transition_matrix=jc):
+class Sequence():
+    """The Sequence object represents elements of a genomic sequence. It
+       knows the DNA sequence and thus the corresponding RNA and
+       protein sequences as well. Additionally, the sequence object
+       can track the mutation rate and a transition matrix to make
+       mutations. By default the mutation rate is 10**-9
+       errors/site/generation and the transition matrix uses the
+       Jukes-Cantor mutation model (i.e. mutation uniform
+       probabilities)."""
+    
+    def __init__(self, inseq, mutation_rate=10**-9, transition_matrix=JC):
         self.dna = inseq
         self.rna = ''
         self.protein = ''
@@ -38,7 +47,8 @@ class sequence():
 
     def identify_ORF(self, inseq):
         """Finds the start and stop position of the longest open reading frame
-        (ORF) for the DNA sequence"""
+           (ORF) for the DNA sequence
+        """
         
         orf_regex = r'(ATG(?:...)*?(?=TAG|TGA|TAA))'
         match_pos = re.finditer(orf_regex, inseq)
@@ -58,9 +68,9 @@ class sequence():
     def is_functional(self, orig_seq, mut_seq):
         orig_start, orig_stop, orig_len = self.identify_ORF(orig_seq)
         mut_start, mut_stop, mut_len = self.identify_ORF(mut_seq)
-        return orig_start == mut_start and \
-            orig_stop == mut_stop and \
-            mut_len > 0
+        return orig_start == mut_start \
+            and orig_stop == mut_stop \
+            and mut_len > 0
 
     def gen_next_seq(self):
         mut_seq = self.make_mutation()
@@ -72,43 +82,13 @@ class sequence():
     def __str__(self):
         return self.dna
 
-
     
-def generate_seq(nlen, training='ACGT'):
-    """Generates a sequence of length nlen using a 0th order Markov model
-       with nucleotide probability probs. Returns a string. """
-
-    MC = mc.markov_seq(0, training)
-    MC.gen_next_state(nlen)
-    return MC.get_chain()
 
 
-x = sequence('ATG' + generate_seq(99) + 'TGA')
-print x
-print Counter(x.dna) # proper proportions?
+#x = Sequence('ATG' + generate_seq(99) + 'TGA')
+#print x
+#print Counter(x.dna) # proper proportions?
 
 #print sg.filter_zero_prob([1.0/3, 0, 1.0/3, 1.0/3]) # {0: 1.0/3, 2: 1.0/3, 3: 1.0/3}
 
 #print sg.transition(sg.filter_zero_prob([1.0/3, 0, 1.0/3, 1.0/3]))
-
-
-
-
-# def identify_ORF(inseq):
-#     """Finds the start and stop position of the longest open reading frame
-#     (ORF) for the DNA sequence"""
-        
-#     orf_regex = r'(ATG(?:...)*?(?=TAG|TGA|TAA))'
-#     match_pos = re.finditer(orf_regex, inseq)
-#     start_pos = 0
-#     stop_pos = 0
-#     max_delta = 0
-#     for m in match_pos:
-#         tmp_start_pos = m.start()
-#         tmp_stop_pos = m.end()
-#         tmp_delta = tmp_stop_pos - tmp_start_pos
-#         if tmp_delta > max_delta:
-#             start_pos = tmp_start_pos
-#             stop_pos = tmp_stop_pos
-#             max_delta = tmp_delta
-#     return start_pos, stop_pos, max_delta
